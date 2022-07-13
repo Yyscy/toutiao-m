1308b240670e12b0589c151c542fce02fde1bf0d
SyrianSoldier <9975545+syriansoldier@user.noreply.gitee.com>
2022-07-13 12:05:27 +0800

    完成项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <template>
   <div>
     <h1>黑马头条</h1>
+    <span class="toutiao toutiao-dianzan2"></span>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <div class="login"></div>
+
+    <div style="background: gold; height: 200px"></div>
   </div>
 </template>
 
 <script>
 export default {
-  data () {}
+  data () {
+    return {}
+  }
 }
 </script>
 
-<style></style>
+<style scoped>
+.login {
+  width: 694px;
+  height: 88px;
+  background-color: #6db4fb;
+  border-radius: 10px;
+}
+</style>
