e312be91bd09aad0dc2b4be0ad41e3cc71fdd312
Yyscy <2103374630@qq.com>
2022-07-13 12:16:07 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入字体图标
+import '@/assets/iconfont/iconfont.css'
+// 动态加载rem单位
+import 'amfe-flexible'
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
